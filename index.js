3ae5aaf5fef487c99aef1f39b3cff041c1f1fab8
ck9fas5 <148320931+ck9fas5@users.noreply.github.com>
2024-04-16 15:30:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const socket = require("socket.io");
+
+let list_user = [];
+let list_message = [];
+
+const server = http.createServer(app);
+const io = new socket.Server(server);
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.static("public"));
+
+app.post("/check_user", (req, res) => {
+  let name = req.body.name;
+  console.log(name);
+  if (list_user.find((n) => n == name) === undefined) {
+    list_user.push(name);
+    res.json({ result: "ok" });
+  } else res.json({ result: "occupato" });
+});
+
+app.get("/get_message", (req, res) => {
+  res.json({ result: list_message });
+});
+
+io.on("connection", (socket) => {
+  console.log("new user");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("message", (message) => {
+    let date = new Date();
+    let year = date.getFullYear();
+    let mounth = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let minut = date.getMinutes();
+    message["time"] =
+      hour + ":" + minut + " " + day + "/" + mounth + "/" + year;
+    list_message.push(message);
+    io.emit("update", message);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("server running");
+});
---
+++ public/src/index.js
@@ -0,0 +1,53 @@
+import { RenderMassege, RenderMassegeList } from "./render.js";
+import { userCheck, getMessage } from "./connection.js";
+
+const login_div = document.getElementById("login");
+const message_div = document.getElementById("message");
+const message_area = document.getElementById("message_area");
+const message_input = document.getElementById("message_input");
+
+const nome_in = document.getElementById("nome");
+const send_btn = document.getElementById("send");
+
+let name_user;
+const socket = io();
+
+socket.on("update", (mes) => {
+  //console.log(mes);
+  message_area.innerHTML = RenderMassege(message_area, mes, name_user);
+  message_area.scrollTop = message_area.scrollHeight;
+});
+
+document.getElementById("entra").onclick = async () => {
+  name_user = nome_in.value;
+  let result = await userCheck(name_user);
+  //console.log(result);
+  if (result.result === "ok") {
+    let text_result = await getMessage();
+    console.log(text_result);
+    message_area.innerHTML = RenderMassegeList(
+      message_area,
+      text_result.result,
+      name_user,
+    );
+    message_area.scrollTop = message_area.scrollHeight;
+    login_div.classList.add("d-none");
+    message_div.classList.remove("d-none");
+  }
+};
+
+send_btn.onclick = () => {
+  let message = {
+    name: name_user,
+    text: message_input.value,
+  };
+  message_input.value = "";
+  //console.log(message);
+  socket.emit("message", message);
+};
+
+message_input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    send_btn.click();
+  }
+});
